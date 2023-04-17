18c89221c77b3d526bfda5dfd7b18222480f0b03
Ubuntu <ubuntu@pa8729-vm.js2local>
2023-04-17 22:20:35 +0000

    added routes

--- src/prognosis_api.py
+++ src/prognosis_api.py
@@ -4,23 +4,60 @@ import requests
 import json
 import csv
 import matplotlib.pyplot as plt
-import pandas as pd
 
 app = Flask(__name__)
 
-r = requests.get('https://raw.githubusercontent.com/dhannywi/Prognosis_API/main/wdbc.data.csv')
-def make_json():
-    data = {}
-    
 
 @app.route('/data', methods = ['POST', 'GET', 'DELETE'])
 def breast_cancer_data() -> dict:
     if request.method == 'POST':
-        r = requests.get(url='https://github.com/dhannywi/Prognosis_API/blob/main/wdbc.data.csv')
-        df = pd.read_csv(r)
-        df.to_json (r'/csvtojsontest.json')
+        r = requests.get(url='https://raw.githubusercontent.com/dhannywi/Prognosis_API/main/wdbc.data.csv')
+        data = {}
+        data['breast_cancer_data'] = []
+        csv_data = r.content.decode('utf-8')
+        csv_reader = csv.DictReader(csv_data.splitlines())
+        #json_data = json.dumps([row for row in csv_reader], ensure_ascii=False)
+        for row in csv_reader:
+            data['breast_cancer_data'].append(dict(row))
+        return f'Data loaded in\n'
+    elif request.method == 'GET':
 
 
 
+    elif request.method == 'DELETE':
+            
+
+
+    else:
+        return f'No available method selected. Methods available: POST, GET, DELETE\n', 404
+
+
+
+
+@app.route('/data/id', methods = ['GET'])
+def breast_cancer_id() -> dict:
+    
+
+
+@app.route('/data/id/<id_num>', methods = ['GET'])
+def id_data() -> dict:
+
+
+
+
+
+
+@app.route('/image', methods = ['GET'])
+def create_imaget():
+
+
+
+
+
+@app.route('/help', methods = ['GET'])
+def all_routes() -> str:
+    
+
+    
 if __name__ == '__main__':
     app.run(debug = True, host = '0.0.0.0')
