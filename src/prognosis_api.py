1052ab5edb5b7ccc00f25559fb092b85263f8b7f
Ubuntu <ubuntu@pa8729-vm.js2local>
2023-04-18 03:32:12 +0000

    added redis-db

--- src/prognosis_api.py
+++ src/prognosis_api.py
@@ -3,17 +3,18 @@ import redis
 import requests
 import json
 import csv
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import os
 
 app = Flask(__name__)
 
 
 def get_redis_client(db_num: int):
-    redis_ip = os.environ.get('REDIS_IP')
-    if no redis_ip:
-        raise Exception()
-    return redis.Redis(host=redis_ip, port=6379, db=db_num, decode_responses=True)
+    #redis_ip = os.environ.get('REDIS_IP')
+    #if no redis_ip:
+    #    raise Exception()
+    #return redis.Redis(host='redis_ip', port=6379, db=db_num, decode_responses=True)
+    return redis.Redis(host='redis-db', port=6379, db=db_num, decode_responses=True)
 
 rd0 = get_redis_client(0)
 rd1 = get_redis_client(1)
@@ -22,7 +23,7 @@ rd1 = get_redis_client(1)
 def get_method() -> dict:
     global rd0
     try:
-        return json.loads(rd0.get('breast_cancer_cases')
+        return json.loads(rd0.get('breast_cancer_cases'))
     except Exception as err:
         return f'Error. Data not loaded in\n', 404
 
@@ -30,7 +31,6 @@ def get_method() -> dict:
 def breast_cancer_data() -> dict:
     global rd0
     if request.method == 'POST':
-
         try:
             r = requests.get(url='https://raw.githubusercontent.com/dhannywi/Prognosis_API/main/wdbc.data.csv')
             data = {}
@@ -39,7 +39,7 @@ def breast_cancer_data() -> dict:
             csv_reader = csv.DictReader(csv_data.splitlines())
             for row in csv_reader:
                 data['breast_cancer_data'].append(dict(row))
-            rd0.set('breast_cancer_cases', json.dumps(data['brease_cancer_data']))
+            rd0.set('breast_cancer_cases', json.dumps(data['breast_cancer_data']))
         except Exception as err:
             return f'Error. Data not loaded in\n', 404
         return f'Data loaded in\n'
@@ -57,12 +57,12 @@ def breast_cancer_data() -> dict:
 
 @app.route('/data/id', methods = ['GET'])
 def breast_cancer_id() -> dict:
-    
+    return f'test'
 
 
 @app.route('/data/id/<id_num>', methods = ['GET'])
 def id_data() -> dict:
-
+    return f'test'
 
 
 
@@ -70,14 +70,14 @@ def id_data() -> dict:
 
 @app.route('/image', methods = ['GET'])
 def create_imaget():
-
+    return f'test'
 
 
 
 
 @app.route('/help', methods = ['GET'])
 def all_routes() -> str:
-    
+    return f'test'
 
     
 if __name__ == '__main__':
