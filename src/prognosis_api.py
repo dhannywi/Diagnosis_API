e96fff8182541f04cff91fb6af4a962d8ecd5a77
Ubuntu <ubuntu@pa8729-vm.js2local>
2023-04-19 19:57:00 +0000

    add image route

--- src/prognosis_api.py
+++ src/prognosis_api.py
@@ -3,21 +3,17 @@ import redis
 import requests
 import json
 import csv
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import os
+import yaml
 
 app = Flask(__name__)
 
-
-def get_redis_client(db_num: int):
-    #redis_ip = os.environ.get('REDIS_IP')
-    #if no redis_ip:
-    #    raise Exception()
-    #return redis.Redis(host='redis_ip', port=6379, db=db_num, decode_responses=True)
-    return redis.Redis(host='redis-db', port=6379, db=db_num, decode_responses=True)
-
-rd0 = get_redis_client(0)
-rd1 = get_redis_client(1)
+redis_ip = os.environ.get('REDIS_IP')
+if not redis_ip:
+    raise Exception()
+rd0 = redis.Redis(host=redis_ip, port=6379, db=0, decode_responses=True)
+rd1 = redis.Redis(host=redis_ip, port=6379, db=1)
 
 
 def get_method() -> dict:
@@ -72,7 +68,7 @@ def id_data(id_num: int) -> dict:
     try:
         return json.loads(rd0.get(id_num))
     except Exception as err:
-        return f'Error. ID requested not in data\n', 404
+        return f'Error. ID {id_num} not found in database\n', 404
 
 
 @app.route('/outcome', methods = ['GET'])
@@ -81,10 +77,57 @@ def get_cases() -> dict:
     cases = {"Malignant": {}, "Benign": {}}
 
     return 'Working on it'
-    
-@app.route('/image', methods = ['GET'])
+
+
+@app.route('/image', methods = ['POST', 'GET', 'DELETE'])
 def create_imaget():
-    return f'Incomplete'
+    global rd0, rd1
+    if request.method == 'POST':
+        graph_data = {}
+        if len(rd0.keys()) == 0:
+            return f'Data not loaded into database', 404
+        val_m = 0
+        val_b = 0
+        for key in rd.keys():
+            m_or_b = json.loads(rd0.get(key))['Diagnosis']
+            if m_or_b == 'M':
+                val_m += 1
+            elif m_or_b == 'B':
+                val_b += 1
+        graph_data = {'Malignant': val_m, 'Benign': val_b}
+        diagnosis = list(graph_data.keys())
+        num_diagnosis = list(graph_data.values())
+        plt.bar(diagnosis, num_diagnosis, color = 'maroon', width = .4)
+        plt.xlabel("Diagnosis of Breast Cancer Cases")
+        plt.ylable("Number of Cases")
+        plt.title("Breast Cancer Cases Prognosis")
+        plt.savefig('./cancer_prognosis.png')
+        image_data = open('./cancer_prognosis.png', 'rb').read()
+        rd1.set('image', image_data)
+        return f'Graph successfully saved'
+
+    elif request.method == 'GET':
+        if len(rd0.keys()) == 0:
+            return f'Data not loaded into database', 404
+        try:
+            image_data = rd1.get('image')
+            if image_data is None:
+                return f'Error. No image found', 404
+            with open('./cancer_prognosis.png', 'wb') as file:
+                file.write(image_data)
+            return send_file('./cancer_prognosis.png', mimetype = 'image/png', as_attachment = True)
+        except Exception as err:
+            return f'Error. Unable to fetch image', 404
+
+    elif request.method == 'DELETE':
+        try:
+            rd1.flushdb()
+            return f'Image deleted'
+        except Exception as err:
+            return f'Unable to delete image', 404
+
+    else:
+        return f'No available method selected. Methods available: POST, GET, DELETE\n', 404
 
 
 @app.route('/help', methods = ['GET'])
